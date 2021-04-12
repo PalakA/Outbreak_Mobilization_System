8ffd9eaf8c284450c366ea4f7e7689ee4d35e98f
Palak Agarwal <agarwal.pa@northeastern.edu>
2021-04-11 23:16:02 -0400

    Added Project Base

---
+++ OutbreakMobilizationSystems/src/OMS/CareTaker/CareTaker.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OMS.CareTaker;
+
+/**
+ * @author ajayp
+ * @author nakul
+ * @author palak
+ */
+public class CareTaker {
+    
+}
